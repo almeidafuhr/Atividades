b81532d51288249b0ddd99be0a1c73e1ea49ac78
Guilherme <115669675+almeidafuhr@users.noreply.github.com>
2024-11-03 22:03:08 -0300

    Add files via upload

--- javascript/CRUD/script.js
+++ javascript/CRUD/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     // Adicione seu código aqui
+    
     $("#btnCriar").click(function () {
         var Sku = $("#sku").val();
         var Title = $("#title").val();
@@ -7,57 +8,114 @@ $(document).ready(function() {
         var Price = validarCampoNumerico($("#price").val());
         var Quantity = validarCampoNumerico($("#quantity").val());
         var Total = validarCampoNumerico($("#total").val());
+        var Id = $("#inputRemover").val(); 
 
-        var myJSON = { 
+        $.ajax({
+            url: "https://open-souce.azurewebsites.net/api/product",
+            type: 'POST',
+            contentType: 'apllication/json',
+            data : JSON.stringify({ 
+                "sku": Sku, 
+                "title": Title,
+                "description": Description,
+                "price": Price,
+                "quantity": Quantity,
+                "total": Total  
+            }),
+            
+            success: function(data) {
+                session = data.session;
+            }
+            
+        });
+
+        /*
+        var request = new XMLHttpRequest();
+
+        request.open('POST', 'https://private-anon-c21263861e-sauceapi1.apiary-mock.com/product');
+
+        request.setRequestHeader('Accept', 'application/json');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.setRequestHeader('authentication', '');
+
+        request.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                console.log('Status:', this.status);
+                console.log('Headers:', this.getAllResponseHeaders());
+                console.log('Body:', this.responseText);
+            }
+        };
+
+        var body = {
+            "sku": Sku,
+            "title": Title,
+            "description": Description,
+            "price": Price,
+            "quantity": Quantity,
+            "total": Total
+        };
+
+        request.send(JSON.stringify(body));
+        */         
+    });
+    
+    $("#btnAtualizar").click(function () {
+        var Sku = $("#sku").val();
+        var Title = $("#title").val();
+        var Description = $("#description").val();
+        var Price = validarCampoNumerico($("#price").val());
+        var Quantity = validarCampoNumerico($("#quantity").val());
+        var Total = validarCampoNumerico($("#total").val());
+        var Id = $("#inputRemover").val(); 
+
+        var request = new XMLHttpRequest();
+        request.open('PUT', "https://private-anon-c21263861e-sauceapi1.apiary-mock.com/product/" + Id);
+
+        request.setRequestHeader('Accept', 'text/plain');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.setRequestHeader('authentication', '');
+
+        request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            console.log('Status:', this.status);
+            console.log('Headers:', this.getAllResponseHeaders());
+            console.log('Body:', this.responseText);
+        }
+        };
+
+        var body = {
             "sku": Sku, 
             "title": Title,
             "description": Description,
             "price": Price,
             "quantity": Quantity,
             "total": Total  
-        }; myJSON;
+        };
 
-        //var Envio = JSON.stringify({ myJSON });
-
-        console.log(myJSON);
-
-
-        /*$.post("https://open-souce.azurewebsites.net/api/product", { myJSON }, function(resposta) {
-            console.log(resposta);
-        });*/
+        request.send(JSON.stringify(body));
         
+        /*alert("Atualizar");
+        data = $.get("https://open-souce.azurewebsites.net/api/product");
+        console.log(data);
+        */
+    });
 
-        function validarCampoNumerico(valor) 
-        {
-            if(isNaN(valor)){
-                alert("Campo é numérico");
-                return valor;
+    $("#btnExcluir").click(function () { 
+        var request = new XMLHttpRequest();
+        request.open('DELETE', "https://private-anon-c21263861e-sauceapi1.apiary-mock.com/product?productId=" + id);
+        request.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                console.log('Status:', this.status);
+                console.log('Headers:', this.getAllResponseHeaders());
+                console.log('Body:', this.responseText);
             }
-            else
-                alert("Campo não é numérico");
-        }
-        
-    });
-    
-    $("#btnAtualizar").click(function () {
-        alert("Atualizar");
-        $.get("https://open-souce.azurewebsites.net/api/product", function(data) {
-            console.log(data);
-        });
-    });
-
-    $("#btnExcluir").click(function () {
-        alert("Excluir");
-        
+        };
+        request.send();
     });
 
 
 
-
-
-
-
-
+    //Alterar fundo dos input texts
     $(".texts").focusin(function() {
         $(this).css("background-color", "azure");
     });
@@ -65,4 +123,15 @@ $(document).ready(function() {
     $(".texts").focusout(function() {
         $(this).css("background-color", "white");
     });
+
+    //Validar campos numéricos
+    function validarCampoNumerico(valor) 
+        {
+            if(isNaN(valor) || valor === "")
+                console.log("Campo não é numérico ou não foi preenchido");
+            else{
+                console.log("Campo é numérico " + valor);
+                return valor;
+            }  
+        }
 });
